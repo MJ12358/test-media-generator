77b43e5e178de0ab65387656b9552642afafdab0
MJ12358 <wtpltlf.dev@gmail.com>
2026-03-11 12:58:08 -0500

    optimize encoder mapper

--- bin/video/encoder_mapper.dart
+++ bin/video/encoder_mapper.dart
@@ -1,54 +1,53 @@
 part of video;
 
+/// This class maps a codec and backend to the
+/// appropriate encoder name and filter to use with FFmpeg.
+/// It also provides a method to check if an encoder is a CPU encoder,
+/// which can be useful for determining if hardware acceleration is being used.
 class EncoderMapper {
-  static String getName(Codec codec, Backend backend) {
-    if (codec is MJPEG) {
-      return 'mjpeg';
+  /// Gets the encoder name for the given codec and backend.
+  static String select(Codec codec, Backend backend) {
+    final String? encoder = codec.hardwareEncoders[backend.runtimeType];
+
+    if (encoder != null) {
+      return encoder;
     }
-    if (codec is MPEG2) {
-      return 'mpeg2video';
-    }
 
-    final String key = '${codec.name}:${backend.name}';
-    switch (key) {
-      // Nvidia
-      case 'h264:nvidia':
-        return 'h264_nvenc';
-      case 'h265:nvidia':
-        return 'hevc_nvenc';
-      case 'av1:nvidia':
-        return 'av1_nvenc';
+    log.w(
+      'No ${codec.name} encoder for ${backend.name}, '
+      'falling back to CPU.',
+    );
 
-      // Intel
-      case 'h264:intel-qsv':
-        return 'h264_qsv';
-      case 'h265:intel-qsv':
-        return 'hevc_qsv';
-      case 'av1:intel-qsv':
-        return 'av1_qsv';
+    return codec.softwareEncoder;
+  }
 
-      // VAAPI
-      case 'h264:vaapi':
-        return 'h264_vaapi';
-      case 'h265:vaapi':
-        return 'hevc_vaapi';
-      case 'av1:vaapi':
-        return 'av1_vaapi';
-      case 'vp9:vaapi':
-        return 'vp9_vaapi';
-
-      // CPU
-      case 'h264:cpu':
-        return 'libx264';
-      case 'h265:cpu':
-        return 'libx265';
-      case 'vp9:cpu':
-        return 'libvpx-vp9';
-      case 'av1:cpu':
-        return 'libaom-av1';
-
-      default:
-        throw UnsupportedException.codecBackend(key);
+  /// Gets the filter for the given codec and backend.
+  static String? getFilter(String encoder, Backend backend) {
+    if (isCpuEncoder(encoder)) {
+      return null;
+    } else {
+      return backend.filter;
     }
   }
+
+  /// Checks if the given encoder is a CPU encoder.
+  static bool isCpuEncoder(String encoder) {
+    return _cpuEncoders.contains(encoder);
+  }
+
+  /// Checks if the given encoder requires the -strict -2 flag.
+  static bool needsStrict(String encoder) {
+    return encoder == 'libaom-av1';
+  }
+
+  /// A set of known CPU encoders.
+  /// This is used to determine if an encoder is a CPU encoder or not.
+  static const Set<String> _cpuEncoders = <String>{
+    'libx264',
+    'libx265',
+    'libvpx-vp9',
+    'libaom-av1',
+    'mpeg2video',
+    'mjpeg',
+  };
 }
